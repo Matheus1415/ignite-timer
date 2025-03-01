b33784a1195721cfb2851e5bed924d3e3307d49f
Matheus <mps.web.tech@gmail.com>
2025-03-01 14:38:50 -0300

    Criação do projeto
    1 - Instalando dependências
    2 - Estruturando pasta

---
+++ src/page/History.tsx
@@ -0,0 +1,6 @@
+
+export function History() {
+    return(
+        <h1>Page History</h1>
+    )
+}
---
+++ src/router/index.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../page/Home";
+import { History } from "../page/History";
+import { DefaultLayout } from "../layout/DefaultLayout";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout/>}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
+    </Routes>
+  );
+}
