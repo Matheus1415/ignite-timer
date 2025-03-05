ea98cc915d301e44c491aa76f4d07a1f52598eb3
Matheus <mps.web.tech@gmail.com>
2025-03-04 22:21:06 -0300

    Projeto completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
-import { BrowserRouter } from "react-router-dom";
-import { Router } from "./router";
-import { CyclesContextProvaider } from "./context/cyclesContext";
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
   return (
-    <>
-      <ThemeProvider theme={defaultTheme}>
-        <BrowserRouter>
-        <CyclesContextProvaider>
-          <Router/> 
-        </CyclesContextProvaider>
-        </BrowserRouter>
-        <GlobalStyle/>
-      </ThemeProvider>
-    </>
-  );
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,23 @@
-import { HeaderContainer } from "./styles";
-import logo from "../../assets/Logo.svg";
-import { Scroll, Timer } from "phosphor-react";
-import { NavLink } from "react-router-dom";
+import { HeaderContainer } from './styles'
+import { Scroll, Timer } from 'phosphor-react'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { NavLink } from 'react-router-dom'
+
 export function Header() {
   return (
     <HeaderContainer>
-      <img src={logo} alt="" />
+      <span>
+        <img src={logoIgnite} alt="" />
+      </span>
       <nav>
         <NavLink to="/" title="Timer">
-            <Timer/>
+          <Timer size={24} />
         </NavLink>
-        <NavLink to="/history" title="History">
-            <Scroll/>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
         </NavLink>
       </nav>
     </HeaderContainer>
-  );
+  )
 }
